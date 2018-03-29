76a06375d035c33688661a5cf2364735bd5a65f3
Tellon Smith <tellonsmith@gmail.com>
2018-03-29 13:16:25 -0500

    Add project files.

---
+++ VideoRentalWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VideoRentalWebApp.MvcApplication" Language="C#" %>
---
+++ VideoRentalWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoRentalWebApp.Startup))]
+namespace VideoRentalWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
