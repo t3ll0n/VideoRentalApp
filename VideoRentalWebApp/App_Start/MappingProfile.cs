a9fd867c322942f74173b37b631863d2b975af75
t3ll0n <tellonsmith@gmail.com>
2018-04-01 17:56:04 -0500

    added Web Api endpoints for customers and movies

---
+++ VideoRentalWebApp/App_Start/MappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using VideoRentalWebApp.Dtos;
+using VideoRentalWebApp.Models;
+
+namespace VideoRentalWebApp.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+            // Domain to Dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<MovieDto, Movie>();
+        }
+
+    }
+}
